8a642df43f059660086e1c48ca57b76ba5ece386
trianggautama <trianggategarutama@gmail.com>
2022-04-07 20:39:58 +0800

    make css global with js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import globalStyle from "../styles/globalStyle";
 
 const Layout = ({ children }) => {
     return (
         <div>
+            <style jsx> {globalStyle}</style>
             <Navbar type="main" />
             {children}
             <Footer/>
