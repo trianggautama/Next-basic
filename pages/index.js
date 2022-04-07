bbb8d16b255c0c783cca6f7232aba672f313fbb3
trianggautama <trianggategarutama@gmail.com>
2022-04-07 21:27:23 +0800

    image-component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { Fragment } from "react";
 const index = () => {
   return (
@@ -12,6 +13,8 @@ const index = () => {
         <section class="p-1 space-y-2">
           <h1 className="font-bold text-blue-400">Home</h1>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <Image src="/1.jpg" alt="desc"  width="100vw" height="100%" quality="100" layout="responsive" objectFit="fill" objectPosition="center center"/>
+          <br></br>
           <a>Google Link</a>
         </section>
       </Fragment>
